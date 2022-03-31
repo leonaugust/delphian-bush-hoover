d4642272a50c5dd2575abdb91bfbd354292b9618
Leon Galushko <leon-galushko@mail.ru>
2022-03-31 20:04:12 +0300

    Initial commit

---
+++ src/main/java/com/delphian/bush/hoover/HooverApplication.java
@@ -0,0 +1,13 @@
+package com.delphian.bush.hoover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HooverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HooverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/delphian/bush/hoover/HooverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delphian.bush.hoover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HooverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
